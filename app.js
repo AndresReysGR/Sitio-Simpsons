6e94eae6b12797b10278b4ab4f209932c58f295c
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-10-08 12:52:10 -0700

    Errors funcionando

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const simpsonsName=document.getElementById('simpson-nombre');
+const simpsonsLast=document.getElementById('simpson-apellido');
+const simpsonsSname=document.getElementById('simpson-snombre');
+const simpsonsAge=document.getElementById('simpson-edad');
+
 const apiUrl ="https://localhost:5001/simpsons/Character";
 const apiRespons = async url =>{
     const response = await fetch(url);
@@ -5,4 +10,20 @@ const apiRespons = async url =>{
     console.log(data);
 
 }
-apiRespons(apiUrl);
+
+const getSimpsonsDatal = async ()=> {
+    const response =await fetch(`${apiUrl}`);
+    console.log(response);
+    const simpson = await response.json();
+    console.log(simpson);
+    const{firstName, age, lastName, secondName} = simpson;
+    simpsonsName.innerText = firstName;
+    simpsonsAge.innerText = age;
+    simpsonsLast.innerText = lastName;
+    simpsonsSname.innerText = secondName;
+
+    console.log(firstName);
+
+}
+//apiRespons(apiUrl);
+getSimpsonsDatal();
